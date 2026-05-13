72557bc521f6b2f6455fc3121cb1048a37db109c
Satish <satishdahare88@gmail.com>
2026-05-13 10:42:40 +0530

    Upload configuration and database setup

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from app.core.config import DATABASE_URL
+from app.db.session import Base, engine
+
+print(DATABASE_URL)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Ecommerce API")
 
 @app.get("/")
 def root():
     return {"message": "Ecommercr API is running"}
+
+
 
